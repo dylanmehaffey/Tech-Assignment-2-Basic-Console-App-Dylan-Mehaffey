2e0f15729aefcb0344ded073ac91809946af31b6
Dylan Mehaffey <dylan36@usf.edu>
2022-02-10 18:55:13 -0500

    Attempting to push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_2_Basic_Console_App_Dylan_Mehaffey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# is a very fun language to learn! - Dylan Mehaffey");
+        }
+    }
+}
